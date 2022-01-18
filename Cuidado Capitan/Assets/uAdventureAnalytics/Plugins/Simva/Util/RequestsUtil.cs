10357fdc79b035f14fc995c28351ba3ec22f9aa0
Xexu98 <jesusf05@ucm.es>
2022-01-18 16:18:15 +0100

    Cambios en sprites y la build

--- Cuidado Capitan/Assets/uAdventureAnalytics/Plugins/Simva/Util/RequestsUtil.cs
+++ Cuidado Capitan/Assets/uAdventureAnalytics/Plugins/Simva/Util/RequestsUtil.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 using UnityEngine.Networking;
 using UnityFx.Async;
 using UnityFx.Async.Promises;
-#if UNITY_ANDROID || UNITY_IOS 
-using Vuopaja;
-#endif
 
 namespace Simva
 {
@@ -47,21 +44,7 @@ namespace Simva
             UnityWebRequestAsyncOperation asyncRequest;
             string backgroundError = null;
 
-#if UNITY_ANDROID || UNITY_IOS
-            if (inBackground)
-            {
-                var webRequestWrapper = new WebRequestWrapper();
-                webRequestWrapper.Failed += (wrappedRequest, error) =>
-                {
-                    backgroundError = error;
-                };
-                asyncRequest = webRequestWrapper.Send(webRequest, 100);
-            }
-            else
-            {
-                asyncRequest = webRequest.SendWebRequest();
-            }
-#else
+
             if (inBackground)
             {
                 Application.runInBackground = true;
@@ -74,7 +57,7 @@ namespace Simva
                     Application.runInBackground = false;
                 };
             }
-#endif
+
 
             if (webRequest.uploadHandler != null)
             {
